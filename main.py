047758779a8e383d5b64aca4ad1ac07d51a7b815
Rishav Sharma <rsrishav0@gmail.com>
2021-05-17 00:22:43 +0530

    2.1.0

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from bs4 import BeautifulSoup
+import os
+import shutil
 import requests
 import pandas as pd
+from datetime import datetime
+from bs4 import BeautifulSoup
+from kaggle import KaggleApi as kag_api
 
+__version__ = "2.1.0"
 MAIN_PAGE_URL = "https://worldpopulationreview.com/"
+CURRENT_YEAR = 0000
+FILE_NAME = ""
+DATA_FOLDER = "dataset"
+
+
+def clear_dir(folder):
+    """ Remove all the files in a folder. """
+    for filename in os.listdir(folder):
+        if filename == 'dataset-metadata.json':
+            pass
+        else:
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+                print(f"[INFO] File removed: {filename}")
+            except Exception as e:
+                print('[ERROR] Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 def get_html_doc(url):
@@ -11,10 +36,13 @@ def get_html_doc(url):
     return response.text
 
 
-def get_country():
+def get_data():
+    """ Scrap data from website. """
+    global CURRENT_YEAR, FILE_NAME
+
     final_data = list()
     key_dict = list()
-    matched_country_data = pd.read_csv("matched_country_list.csv")
+    matched_country_data = pd.read_csv("country_list.csv")
     matched_countries = matched_country_data["location"].tolist()
 
     html_document = get_html_doc(MAIN_PAGE_URL)
@@ -27,25 +55,42 @@ def get_country():
         th_text = th_text.strip().lower().replace(" ", "_").replace("(", "").replace(")", "")
         if th_text == "flag":
             th_text = "iso_code"
+        if th_text.__contains__("live"):
+            th_text = th_text.replace("live", "last_updated")
+            CURRENT_YEAR = th_text[:4]
+        if th_text.__contains__("km²"): th_text = th_text[:len(th_text) - 3] + "sq_km"
         key_dict.append(th_text)
 
     for trow in table_body.find_all("tr"):
         row_data = list()
         country_name_tag = trow.find(href=True)
         country_name = country_name_tag.text
-
         if country_name in matched_countries:
             for td in trow:
                 text = td.get_text()
                 if text == "":
-                    iso_code = matched_country_data[matched_country_data["location"] == country_name]["iso_code"].iloc[0]
+                    iso_code = matched_country_data[matched_country_data["location"] == country_name]["iso_code"].iloc[
+                        0]
                     text = iso_code
+                if text.__contains__("km²"): text = text[:len(text) - 3] + "sq_km"
                 row_data.append(text)
             final_data.append(row_data)
 
+    FILE_NAME = os.path.join(DATA_FOLDER, f"{CURRENT_YEAR}_population.csv")
     df = pd.DataFrame(data=final_data, columns=key_dict)
-    df.to_csv('final.csv')
+    df.to_csv(FILE_NAME, index=False)
+
+
+def publish_data():
+    api = kag_api()
+    kag_api.authenticate(api)
+    print("[INFO] Kaggle credentials authenticated.")
+    response = kag_api.dataset_create_version(api, DATA_FOLDER, f"Dataset updated till (UTC): {datetime.utcnow()}",
+                                              convert_to_csv=True, delete_old_versions=False)
+    print(f"[INFO] Kaggle Dataset uploaded.")
+    clear_dir(DATA_FOLDER)
 
 
 if __name__ == '__main__':
-    get_country()
+    get_data()
+    publish_data()
